b78cd338d6f1c32bcabf7d959cbd90f53883a2fe
vamsi-afk <56501742+vamsi-afk@users.noreply.github.com>
2021-01-20 23:01:08 +0530

    Update unprotect.py

--- unprotect.py
+++ unprotect.py
@@ -2,11 +2,11 @@ import win32com.client as win32
 
 word = win32.gencache.EnsureDispatch('Word.Application')
 word.Visible = False
-doc = word.Documents.Open("C:\\Users\\pc\\Desktop\\Word_Automations\\protected.docx")
+doc = word.Documents.Open("fullpathofyourdoc.docx")
 
 password= input("Please enter the password: ")
 
 doc.Unprotect(password)                      
 print("Protection Removed!")
 
-doc.SaveAs("C:\\Users\\pc\\Desktop\\Word_Automations\\unprotected.docx")
+doc.SaveAs("fullpathofyourdoc.docx")
