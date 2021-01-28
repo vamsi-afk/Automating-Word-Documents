f7bcbd9c2dbac92bac42a5882049886eb660253e
vamsi-afk <56501742+vamsi-afk@users.noreply.github.com>
2021-01-28 14:40:08 +0530

    Add files via upload

--- Capital_words_highlight.py
+++ Capital_words_highlight.py
@@ -6,7 +6,12 @@ word.Visible = True
 doc = word.Documents.Open("C:\\Users\\VAMSI\\Desktop\\as\\oof.docx")
 words = []
 
-for word in doc.Words:
-     t = re.findall('([A-Z][a-z]+)', str(word))
+for word in doc.Sentences:
+    x = (str(word).partition(' ')[0])
+    words.append(x)
+for word_1 in doc.Words:
+     t = re.findall('([A-Z][a-z]+)', str(word_1))
      if(t):
-         word.HighlightColorIndex = 7
+          if (t[0] not in words):
+               print(t)
+               word_1.HighlightColorIndex = 7
