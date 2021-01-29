865ce08ff7c332f57b10128c7e16bdeb4f5cbe48
vamsi-afk <56501742+vamsi-afk@users.noreply.github.com>
2021-01-29 12:04:51 +0530

    Update prefix_and_hyphenated_words.py

--- prefix_and_hyphenated_words.py
+++ prefix_and_hyphenated_words.py
@@ -2,7 +2,7 @@ import win32com.client as win32
 import re
 word = win32.gencache.EnsureDispatch('Word.Application')
 word.Visible = True
-doc = word.Documents.Open("C:\\Users\\krupa\\Desktop\\20101.docx")
+doc = word.Documents.Open("C:\\Users\\VAMSI\\Desktop\\as\\oof.docx")
 
 
 result = []
@@ -10,7 +10,7 @@ result2 = []
 for word in doc.Sentences:
     result1 = re.findall(r'\w+(?:-\w+)+', str(word))
     result = result + result1
-    result3 = re.findall(i.e, str(word))
+    result3 = re.findall(r'\w+(?:-+)+', str(word))
     result2 = result2 + result3
 print(result)
 print(result2)
