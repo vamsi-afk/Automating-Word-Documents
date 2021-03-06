7eaa400c93c9cc7b36ada2967238811141b09655
Vamsi Krishna S <56501742+vamsi-afk@users.noreply.github.com>
2021-03-06 20:32:34 +0530

    Update eg_and_i_e.py

--- eg_and_i_e.py
+++ eg_and_i_e.py
@@ -3,20 +3,28 @@ import re
 
 word = win32.gencache.EnsureDispatch('Word.Application')
 word.Visible = True
-doc = word.Documents.Open("C:\\Users\\VAMSI\\Desktop\\as\\oof.docx")
+doc = word.Documents.Open("C:\\Users\\VAMSI\\Desktop\\as\\test01.docx")
 words = []
 
-for word in doc.Paragraphs:
-     t = re.findall("\w[.]\w[.][,]", str(word))
+for sentence in doc.Paragraphs:
+     t = re.findall("\w[.]\w[.][,]", str(sentence))
      if(t):
           print(t)
+for sentence in doc.Sentences:
+     x = re.findall("e[.]g",str(sentence))
+     if x:
+          print(x)
           # word.HighlightColorIndex = 7
+for word in doc.Paragraphs:
+     x = re.findall("e[.]g[.]",str(word))
+     if x:
+          print(x)
 for word in doc.Sentences:
      t = re.findall('for example',str(word))
      t1 = re.findall('that is',str(word))
      if t:
           print(t)
-          word.HighlightColorIndex = 7
+          # word.HighlightColorIndex = 7
      if t1:
           print(t1)
-          word.HighlightColorIndex = 7
+          # word.HighlightColorIndex = 7
